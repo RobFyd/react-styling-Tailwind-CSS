aa24723f76cd790c6c5fc2f78d26273b6baafb83
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-12-18 01:52:44 +0000

    Refactor List component to enhance button styling and layout with Tailwind CSS

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -7,10 +7,15 @@ const StyledListItem = styled(Item)`
   margin-bottom: 50px;
 `;
 
+const WIDTH = 120;
+
 export function List() {
   return (
-    <>
-      <button onClick={() => alert(`Number of emojis: ${emojis.length}`)}>
+    <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+      <button
+        className={`bg-transparent border border-solid border-current p-3 rounded-[5px] cursor-pointer w-[${WIDTH}px] text-salmon transition-colors m-8 hover:text-white hover:bg-salmon`}
+        onClick={() => alert(`Number of emojis: ${emojis.length}`)}
+      >
         Show emoji count
       </button>
       <ul className="flex flex-col gap-8 p-8">
@@ -18,6 +23,6 @@ export function List() {
           <Item key={emoji} emoji={emoji} />
         ))}
       </ul>
-    </>
+    </div>
   );
 }
